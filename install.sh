02475b9563767c87acffd985ddfbe96ed4e2821c
Cédric Houbart <cedric@scil.coop>
2012-06-28 11:50:09 +0200

    Make sure launcher is executable after copy

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ DIR=$(pwd)
 
 mv $DIR $INSTALL_DIR
 cp $INSTALL_DIR/pos-tech /usr/bin/
+chmod 755 /usr/bin/pos-tech
 cp $INSTALL_DIR/pos-tech.desktop /usr/share/applications/
 ln -s $INSTALL_DIR/pos-tech-logo-128.png /usr/share/icons/pos-tech.png
