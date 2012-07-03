9e50880b8127a77d6bb7af82ae48a4c4118e66b0
Cédric Houbart <cedric@scil.coop>
2012-07-03 16:21:06 +0200

    Version 1.1.0 beta1 notation

--- src-pos/com/openbravo/pos/forms/AppLocal.java
+++ src-pos/com/openbravo/pos/forms/AppLocal.java
@@ -31,7 +31,7 @@ public class AppLocal {
     
     public static final String APP_NAME = "POS-Tech";
     public static final String APP_ID = "postech";
-    public static final String APP_VERSION = "1.0.0";
+    public static final String APP_VERSION = "1.1.0 beta1";
     public static final String DB_VERSION = "2";
   
     // private static List<ResourceBundle> m_messages;
