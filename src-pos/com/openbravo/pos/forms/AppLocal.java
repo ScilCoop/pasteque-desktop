8fca901629215196dada8916371314ecbd3943c7
Cédric Houbart <cedric@scil.coop>
2012-09-14 14:28:14 +0200

    Verstion 1.2_alpha1 notation

--- src-pos/com/openbravo/pos/forms/AppLocal.java
+++ src-pos/com/openbravo/pos/forms/AppLocal.java
@@ -31,7 +31,7 @@ public class AppLocal {
     
     public static final String APP_NAME = "POS-Tech";
     public static final String APP_ID = "postech";
-    public static final String APP_VERSION = "1.2";
+    public static final String APP_VERSION = "1.2_alpha1";
     public static final String DB_VERSION = "3";
   
     // private static List<ResourceBundle> m_messages;
