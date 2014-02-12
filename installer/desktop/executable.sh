4dd4f6e23e07509c363539f0204d59115719ca2d
Cédric Houbart <cedric@scil.coop>
2014-02-12 16:33:14 +0100

    Fix uninstaller, remove documentation pack

--- installer/desktop/executable.sh
+++ installer/desktop/executable.sh
@@ -4,5 +4,5 @@ execPath=$(readlink -f $(dirname $0))
 chmod 700 $execPath/start.sh
 chmod 700 $execPath/configure.sh
 touch $execPath/uninstall.sh
-command echo "java -jar $execPath/Pasteque-uninstaller.jar" >> $execPath/uninstall.sh
+command echo "java -jar $execPath/uninstall.jar" >> $execPath/uninstall.sh
 chmod 700 $execPath/uninstall.sh
